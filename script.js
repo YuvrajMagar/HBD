25282a34b3307de3542d6cf5d8897894a3d0772e
YuvrajMagar <73734662+YuvrajMagar@users.noreply.github.com>
2021-03-26 08:59:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var a=prompt("Enter UserName");
  var b=prompt("Enter password");
-  	if(a== "Yuvraj" && b=="31-12-2006")
+  	if(a== "yuvraj" && b=="yuvraj")
   	{
   	    window.location.href="start.html";
   	}
