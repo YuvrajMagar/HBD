3041caffcbc50e94a2c2e8f2e69de107e1c1a8c2
YuvrajMagar <73734662+YuvrajMagar@users.noreply.github.com>
2021-09-24 20:00:59 +0530

    Update script.js
    Updated for pika

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var a=prompt("Enter UserName");
  var b=prompt("Enter password");
-  	if(a== "yuvraj" && b=="yuvraj")
+  	if(a== "Carolina Angel Rai" && b=="24/09/2021")
   	{
   	    window.location.href="start.html";
   	}
